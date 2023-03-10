11d424d4478ae414e2dbdd1abec21c880a2e63a2
AlbertoArochal <arochaldev@gmail.com>
2023-03-10 07:28:35 +0100

    initial commit

---
+++ src/components/counter.vue
@@ -0,0 +1,81 @@
+<!-- <template>
+    <h2>{{ customTitle }}</h2>
+    <p>{{ count }} <sup>2</sup> = {{ getSquareValue() }}</p>
+    <div>
+        <button @click="decrement">Decrement</button>
+        <button @click="increment">Increment</button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        title: String,
+        start: {
+            type: Number,
+            default: 101,
+            validator(value: number) {
+                return value >= 100;
+            },
+        },
+    },
+    name: "Counter-Vue",
+    data() {
+        return {
+            count: this.start,
+        };
+    },
+    methods: {
+        increment() {
+            this.count++;
+        },
+        decrement() {
+            this.count--;
+        },
+        getSquareValue() {
+            console.log("getSquareValue() called");
+            return this.count * this.count;
+        },
+    },
+    computed: {
+        SquareCounter() {
+            return this.count * this.count;
+        },
+
+        customTitle() {
+            return this.title || "Counter";
+        },
+    },
+};
+</script>
+
+<style>
+h2 {
+    font-size: 2em;
+    font-weight: normal;
+}
+p {
+    font-size: 1.2em;
+}
+button {
+    background-color: #2efd95;
+    border-radius: 5px;
+    border: 1px solid #2efd95;
+    font-size: 1em;
+    margin: 5px;
+    padding: 12px;
+}
+button:hover {
+    background-color: #02c765;
+    border: 1px solid #2efd95;
+    cursor: pointer;
+    transform: scale(1.05);
+    transition: all 0.2s ease-in-out;
+}
+button:active {
+    background-color: #2efd95;
+    border: 1px solid #2efd95;
+    transform: scale(0.98);
+    transition: all 0.2s ease-in-out;
+}
+</style> -->
---
+++ src/components/indecision.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <div class="bg-dark">
+            <img src="https://via.placeholder.com/250" alt="bg" />
+            <h1>Indecision</h1>
+        </div>
+        <div class="indecision__container">
+            <input type="text" placeholder="Hazme una pregunta" />
+            <p>Recuerda terminar con un signo de interrogacion</p>
+        </div>
+        <div>
+            <h2>Sere millonario?</h2>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+export default {
+    props: {},
+    name: "Indecision-vue",
+    data() {
+        return "Indecision App";
+    },
+};
+</script>
+<style>
+img,
+.bg-dark {
+    height: 100vh;
+    left: 0px;
+    max-height: 100%;
+    max-width: 100%;
+    position: fixed;
+    top: 0px;
+    width: 100vw;
+}
+
+.bg-dark {
+    background-color: rgba(0, 0, 0, 0.4);
+}
+
+.indecision__container {
+    position: relative;
+    z-index: 99;
+}
+
+input {
+    width: 250px;
+    padding: 10px 15px;
+    border-radius: 5px;
+    border: none;
+}
+input:focus {
+    outline: none;
+}
+
+p {
+    color: white;
+    font-size: 20px;
+    margin-top: 0px;
+}
+
+h1,
+h2 {
+    color: white;
+}
+
+h2 {
+    margin-top: 150px;
+}
+</style>
